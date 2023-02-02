bb8a309bd152db87f7a048bc0e28bcd15c3898af
Akylbek Rysbekov <akylbekrysbekov14@gmail.com>
2023-02-02 20:27:38 +0600

    webpack settings

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './index.html';
+import './index.scss';
+import { sayHello } from './modules/script';
+
+const mult = (a, b) => a + b;
+console.log(mult(10, 7));
+console.log(mult(85, 200));
+sayHello();
