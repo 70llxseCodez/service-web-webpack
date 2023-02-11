0176a6032385f173942588f594bce406968bf9ab
Akylbek Rysbekov <akylbekrysbekov14@gmail.com>
2023-02-11 17:17:14 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
 import './index.html';
 import './index.scss';
-import { sayHello } from './modules/script';
-
-const mult = (a, b) => a + b;
-console.log(mult(10, 7));
-console.log(mult(85, 200));
-sayHello();
+import './modules/slider';
--- src/modules/script.js
+++
@@ -1,3 +0,0 @@
-export const sayHello = async () => {
-  console.log('hello');
-};
